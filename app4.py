44ead6e7f335055a674f716a8e2e10da281365d6
gahye0530 <40849083+gahye0530@users.noreply.github.com>
2021-12-13 16:59:56 +0900

    0459

--- app4.py
+++ app4.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 def main() :
-    st.button('데이터보기')
+
+    df = pd.read_csv('data/iris.csv')
+    if st.button('데이터보기') : st.dataframe(df)
 
 if __name__ == '__main__' : main()
