0d02c8e940f2202df1eb4d433d00383e3c6afec5
gahye0530 <40849083+gahye0530@users.noreply.github.com>
2021-12-13 16:55:19 +0900

    0455

---
+++ app3.py
@@ -0,0 +1,10 @@
+import streamlit as st
+def main() :
+    df = pd.read_csv('iris.csv')
+    st.dataframe(df)
+
+    species = df['spacies'].unique()
+    st.text('아이리스 꽃은' + species + '으로 되어있다.')
+    st.write(df.head())
+
+if __name__=='__main__' : main()
